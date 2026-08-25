f3e2e291fb444727f4fcf97503b5da290f18726e
lthnh <imr3ng4l@gmail.com>
2026-08-25 13:03:52 +0700

    feat(testbench): add immediate_sign_extend testbench

--- tests/tb/immediate_sign_extend_tb.v
+++ tests/tb/immediate_sign_extend_tb.v
@@ -1,7 +1,7 @@
 `include "common.vh"
 `include "assert.vh"
 
-`define N_INSTR_TYPES 4
+`define SAMPLE_SIZE 100
 
 module immediate_sign_extend_tb;
   reg [`WIDTH-1:0] curr_instr_in;
@@ -15,15 +15,35 @@ module immediate_sign_extend_tb;
     .imm_ext_out   (imm_ext_out)
   );
 
-  reg [`WIDTH-1:0] test_instr_int [`N_INSTR_TYPES-1:0] =
+  reg [`WIDTH-1:0] imm_ext_ref;
 
-  initial fork
-    clk_in = 1'b0;
-    forever #1 clk_in = ~clk_in;
-  join
+  always @(*) begin
+    case (imm_src_sel_in)
+      2'b00: imm_ext_ref = {{20{curr_instr_in[31]}}, curr_instr_in[31:20]};
+      2'b01: imm_ext_ref = {{20{curr_instr_in[31]}}, curr_instr_in[31:25], curr_instr_in[11:7]};
+      2'b10: imm_ext_ref = {{20{curr_instr_in[31]}}, curr_instr_in[7], curr_instr_in[30:25], curr_instr_in[11:8], 1'b0};
+      2'b11: imm_ext_ref = {{12{curr_instr_in[31]}}, curr_instr_in[19:12], curr_instr_in[20], curr_instr_in[30:21], 1'b0};
+      default: imm_ext_ref = {`WIDTH{1'b0}};
+    endcase
+  end
 
+  integer i, j;
   initial begin
+    for (i = 0; i < `SAMPLE_SIZE; i = i + 1) begin
+      for (j = 0; j < 4; j = j + 1) begin
+        curr_instr_in = $unsigned($random);
+        imm_src_sel_in = j[1:0];
+        #1;
+        `assert_eq_fmt(imm_ext_out, imm_ext_ref, %d)
+      end
+    end
     $display("%m is passed!");
     $finish;
   end
+
+  initial fork
+    $dumpvars();
+    clk_in = 1'b0;
+    forever #1 clk_in = ~clk_in;
+  join
 endmodule
